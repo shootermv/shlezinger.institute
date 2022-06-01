b6aaf7976bb1c592ee31b26483bc01f1e9f0d86d
moshe vilner <shootermv@gmail.com>
2022-06-01 17:51:24 +0300

    add layout components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,18 @@
 // Step 2: Define your component
-import * as React from 'react'
+import * as React from "react";
+import Layout from "../components/layout";
 
 const AboutPage = () => {
-    return (
+  return (
+    <Layout pageTitle="About">
       <main>
-        <title>About Me</title>
-        <h1>About Me</h1>
-        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <p>
+          Hi there! I'm the proud creator of this site, which I built with
+          Gatsby.
+        </p>
       </main>
-    )
-  }
-  // Step 3: Export your component
-  export default AboutPage
+    </Layout>
+  );
+};
+// Step 3: Export your component
+export default AboutPage;
