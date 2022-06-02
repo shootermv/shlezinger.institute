69728b3cc70d035eb13ef5ac1ca5f73d173c7292
moshe vilner <shootermv@gmail.com>
2022-06-02 09:47:45 +0300

    prettify page title

--- src/pages/article/{mdx.slug}.js
+++ src/pages/article/{mdx.slug}.js
@@ -1,28 +1,26 @@
-
-import * as React from 'react'
-import Layout from '../../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-const Article = ({data}) => {
+import * as React from "react";
+import Layout from "../../components/layout";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+const Article = ({ data }) => {
   return (
-    <Layout pageTitle="Super Cool Article">
-      <p>{data.mdx.frontmatter.date}</p>
-      <MDXRenderer>
-       {data.mdx.body}
-      </MDXRenderer>
+    <Layout pageTitle={`Articles / ${data.mdx.frontmatter.title}`}>
+      <header style={{ display: 'flex' }}>{data.mdx.frontmatter.author} {data.mdx.frontmatter.date}</header>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        author
       }
       body
     }
   }
-  `
-export default Article
+`;
+export default Article;
