e4ac9af8ce0683f3d34e2f070ac2508e16181d2d
moshe vilner <shootermv@gmail.com>
2022-06-08 17:55:54 +0300

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,10 +6,7 @@ const AboutPage = () => {
   return (
     <Layout pageTitle="About">
       <main>
-        <p>
-          Hi there! I'm the proud creator of this site, which I built with
-          Gatsby.
-        </p>
+      The Schlesinger Institute for Medical-Halachic Research was founded in Israel in 1966 under the auspices of Shaare Zedek Medical Center. It is named after the hospital's second director-general. The institute is dedicated to the halachic (Jewish law) approach to medical ethics, and has the purpose of researching and resolving the halachic issues that emerge as medicine progresses, to consider their medical, halachic, legal and ethical ramifications, and to present practical responses.[1] Leading rabbis, physicians and others, consider and respond to issues, with the intention of following the spirit of Torah
       </main>
     </Layout>
   );
