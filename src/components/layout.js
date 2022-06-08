8d4f0611b340dd7864c678ea79ba9bb009e2df7f
moshe vilner <shootermv@gmail.com>
2022-06-08 17:42:43 +0300

    adding third article

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,37 +3,41 @@ import { Link } from "gatsby";
 import {
   container,
   heading,
+  main,
   navLinks,
   navLinkItem,
   navLinkText,
 } from "./layout.module.css";
 import { Menu } from "antd";
 const Layout = ({ pageTitle, children }) => {
-  const url = typeof window !== 'undefined' ? window.location : '';
+  const url = typeof window !== "undefined" ? window.location : "";
 
-  console.log('url-', url?.pathname)
+  console.log("url-", url?.pathname);
   return (
     <div className={container}>
-      <nav style={{marginBottom: '20px'}}>
-      <Menu mode="horizontal" selectedKeys={[url?.pathname?.replace("/", "") || "home"]}>
-        <Menu.Item key="home">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="about">
-          <Link to="/about">About</Link>
-        </Menu.Item>
-        <Menu.Item key="articles">
-          <Link to="/articles">Articles</Link>
-        </Menu.Item>
-        <Menu.Item key="shop">
-          <Link to="/shop">Shop</Link>
-        </Menu.Item>
-        <Menu.Item key="contact">
-          <Link to="/contact">Contact</Link>
-        </Menu.Item>
-      </Menu>
+      <nav style={{ marginBottom: "20px" }}>
+        <Menu
+          mode="horizontal"
+          selectedKeys={[url?.pathname?.replace("/", "") || "home"]}
+        >
+          <Menu.Item key="home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="about">
+            <Link to="/about">About</Link>
+          </Menu.Item>
+          <Menu.Item key="articles">
+            <Link to="/articles">Articles</Link>
+          </Menu.Item>
+          <Menu.Item key="shop">
+            <Link to="/shop">Shop</Link>
+          </Menu.Item>
+          <Menu.Item key="contact">
+            <Link to="/contact">Contact</Link>
+          </Menu.Item>
+        </Menu>
       </nav>
-      <main>
+      <main className={main}>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
@@ -41,4 +45,3 @@ const Layout = ({ pageTitle, children }) => {
   );
 };
 export default Layout;
-
