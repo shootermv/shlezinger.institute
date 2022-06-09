483df33b2f7d0e32f1d1819fe58d65b78d621c67
moshe vilner <shootermv@gmail.com>
2022-06-09 07:26:40 +0300

    add 'more' buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Layout from "../components/layout";
 
-import { Button, Card, Row, Col, Typography } from 'antd'
+import { Button, Card, Row, Col, Typography } from "antd";
+import { Link } from "gatsby";
 // styles
 const pageStyles = {
   color: "#232129",
@@ -12,34 +13,30 @@ const paragraphStyles = {
   marginBottom: 48,
 };
 
-
-
-
-
 // data
 const links = [
   {
     text: "Essays",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
+    url: "/articles",
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
     color: "#1099A8",
   },
   {
     text: "Shop",
-    url: "https://www.gatsbyjs.com/docs/reference/",
+    url: "/shop",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
     color: "#BC027F",
   },
   {
     text: "About Us",
-    url: "https://www.gatsbyjs.com/docs/reference/",
+    url: "/about",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
     color: "#BC027F",
-  }
-]
+  },
+];
 const { Title } = Typography;
 
 // markup
@@ -47,19 +44,24 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Shlezinger Institute">
       <main style={pageStyles}>
-       
         <h3 style={paragraphStyles}>
-         Academic Rresearches For Medicine And Jewish Law
+          Academic Rresearches For Medicine And Jewish Law
         </h3>
 
         <Row gutter={[32, 32]}>
-          
-             {links.map((link, idx) => <Col xs={24} lg={8} key={`item-${idx}`} style={{textAlign: 'center'}}>
-                <Card>
-                  <Title level={2}>{link.text}</Title> 
-                </Card> 
-             </Col>)}
-           
+          {links.map((link, idx) => (
+            <Col
+              xs={24}
+              lg={8}
+              key={`item-${idx}`}
+              style={{ textAlign: "center" }}
+            >
+              <Card>
+                <Title level={2}>{link.text}</Title>
+                <Link to={link.url}><Button type="primary">More</Button></Link>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </main>
     </Layout>
