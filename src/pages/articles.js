e85c49b457434e457c58a01b08a5fda13b9fa5ff
moshe vilner <shootermv@gmail.com>
2022-09-21 22:38:56 +0300

    feat: add articles filter input

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,19 +1,58 @@
 // Step 2: Define your component
 import * as React from "react";
+import { useState } from "react";
 import Layout from "../components/layout";
-import { graphql, Link } from 'gatsby'
+import { graphql, Link } from "gatsby";
+import { Space } from "antd";
 const ArticlesPage = ({ data, location }) => {
+  const emptyQuery = "";
+
+  const [state, setState] = useState({
+    filteredData: [],
+    query: emptyQuery,
+  });
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    const query = event.target.value;
+
+    const posts = data.allMdx.nodes || [];
+
+    const filteredData = posts.filter((post) => {
+      const { title } = post.frontmatter;
+      return title.toLowerCase().includes(query.toLowerCase());
+    });
+
+    setState({
+      query,
+      filteredData,
+    });
+  };
+
+  const allPosts = data.allMdx.nodes;
+  const { filteredData, query } = state;
+  const hasSearchResults = filteredData && query !== emptyQuery;
+  const posts = hasSearchResults ? filteredData : allPosts;
   return (
     <Layout pageTitle="Articles" location={location}>
       <main>
-    
-      {data.allMdx.nodes.map((node) => (
+        <Space direction="vertical">
+        <input
+          type="search"
+          placeholder="Type to filter posts..."
+          onChange={handleInputChange}
+        />
+        {posts.map((node) => (
           <article key={node.id}>
-            <h2> <Link to={`/article/${node.slug}`}>{node.frontmatter.title}</Link></h2>
-            <div>{node.frontmatter.author} {node.frontmatter.date}</div>
+            <h2>
+              {" "}
+              <Link to={`/article/${node.slug}`}>{node.frontmatter.title}</Link>
+            </h2>
+            <div>
+              {node.frontmatter.author} {node.frontmatter.date}
+            </div>
           </article>
-        ))}
-    
+        ))}</Space>
       </main>
     </Layout>
   );
@@ -21,7 +60,10 @@ const ArticlesPage = ({ data, location }) => {
 
 export const query = graphql`
   query {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}, filter: {fileAbsolutePath: {regex: "/(article)/"  }}) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { fileAbsolutePath: { regex: "/(article)/" } }
+    ) {
       nodes {
         frontmatter {
           date(formatString: "MMMM D, YYYY")
@@ -33,6 +75,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 // Step 3: Export your component
 export default ArticlesPage;
